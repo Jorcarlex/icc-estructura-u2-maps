7cd4f80fbd0762c7c61ebf3706d1d6ae212295da
unknown <Jorcarlex>
2025-01-29 12:56:38 -0500

    Diccionarios - Conceptos

--- src/App.java
+++ src/App.java
@@ -18,11 +18,34 @@ public class App {
     }
 
     private static void runEmpleadoExample() {
-        throw new UnsupportedOperationException("Not implemented yet");
+        EmpleadoContoller empleadoContoller = new EmpleadoContoller();
+
+        boolean result = empleadoContoller.addEmpleado(new Empleado(1, "Pablo", "Senior"));
+        System.out.println("Ingreso " + result);
+
+        result = empleadoContoller.addEmpleado(new Empleado(1, "Pablo", "Senior"));
+        System.out.println("Ingreso " + result);
+
+        System.out.println("---------------------");
+
+        empleadoContoller.addEmpleado(new Empleado(1, "Pablo", "Senior"));
+        empleadoContoller.addEmpleado(new Empleado(2, "Juan", "Senior"));
+        empleadoContoller.addEmpleado(new Empleado(3, "Andres", "Senior"));
+
+        System.out.println(empleadoContoller.getEmpleadoById(2));
+
+        empleadoContoller.displayEmpleados();
+        System.out.println("---------------------");
+        empleadoContoller.displayEmpleadosSoloNombres();
+        System.out.println("---------------------");
+        empleadoContoller.displayLlaves();
+        System.out.println("---------------------");
+        empleadoContoller.displayEmpleadosValues();
+
     }
 
     private static void runMapExamlpe() {
-        throw new UnsupportedOperationException("Not implemented yet");
+        new Mapa();
     }
 
     private static void runEjerccios() {
--- src/controllers/Mapa.java
+++ src/controllers/Mapa.java
@@ -1,8 +1,91 @@
 package controllers;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.TreeMap;
 
 public class Mapa {
     public Mapa() {
+        hashMap();
+        linkedHashMap();
+        treeMapa();
+    }
+
+    public void hashMap() {
+        HashMap<String, Integer> mapa = new HashMap<>();
+
+        mapa.put("Manzana", 10);
+        mapa.put("Pera", 11);
+        mapa.put("Durazno", 5);
+        mapa.put("Mango", 6);
+
+        System.out.println("HashMap");
+        System.out.println(mapa);
+
+        // .containsKey() verifica si existe una llave en el mapa
+        System.out.println("Hay Pera " + mapa.containsKey("Pera"));
+        System.out.println("Hay Kiwi " + mapa.containsKey("Kiwi"));
+
+        // Obtener el valor
+        // .get() obtiene el valor de una llave
+        System.out.println("Valor de Pera: " + mapa.get("Pera"));
+
+        // Eliminar un elemento
+        // .remove() elimina un elemento del mapa
+        mapa.remove("Pera");
+        System.out.println(mapa);
+        System.out.println("--------------------");
+    }
+
+    public void linkedHashMap() {
+        LinkedHashMap<String, Integer> mapaLHM = new LinkedHashMap<>();
+
+        mapaLHM.put("Manzana", 10);
+        mapaLHM.put("Pera", 11);
+        mapaLHM.put("Durazno", 5);
+        mapaLHM.put("Mango", 6);
+
+        System.out.println("LinkedHashMap");
+        System.out.println(mapaLHM);
+
+        // .containsKey() verifica si existe una llave en el mapaLHM
+        System.out.println("Hay Pera " + mapaLHM.containsKey("Pera"));
+        System.out.println("Hay Kiwi " + mapaLHM.containsKey("Kiwi"));
+
+        // Obtener el valor
+        // .get() obtiene el valor de una llave
+        System.out.println("Valor de Pera: " + mapaLHM.get("Pera"));
+
+        // Eliminar un elemento
+        // .remove() elimina un elemento del mapa
+        mapaLHM.remove("Pera");
+        System.out.println(mapaLHM);
+        System.out.println("--------------------");
+    }
+
+    // El treeMap es el diccionario de la vida real
+    public void treeMapa() {
+        TreeMap<String, Integer> mapaTM = new TreeMap<>();
+
+        mapaTM.put("Manzana", 10);
+        mapaTM.put("Pera", 11);
+        mapaTM.put("Durazno", 5);
+        mapaTM.put("Mango", 6);
+
+        System.out.println("TreeMap");
+        System.out.println(mapaTM);
+
+        // .containsKey() verifica si existe una llave en el mapaTM
+        System.out.println("Hay Pera " + mapaTM.containsKey("Pera"));
+        System.out.println("Hay Kiwi " + mapaTM.containsKey("Kiwi"));
+
+        // Obtener el valor
+        // .get() obtiene el valor de una llave
+        System.out.println("Valor de Pera: " + mapaTM.get("Pera"));
+
+        // Eliminar un elemento
+        // .remove() elimina un elemento del mapa
+        mapaTM.remove("Pera");
+        System.out.println(mapaTM);
     }
 }
